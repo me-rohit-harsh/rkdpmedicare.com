e5508656d5c6d442763895d55a0e2601ecb30218
Rohit kumar <rohitkumarah369@gmail.com>
2023-08-10 04:54:09 +0530

    Complete working of contact form

--- connect.php
+++ connect.php
@@ -1,20 +1,26 @@
 <?php
-$name=$_POST['name'];
-$email=$_POST['email'];
-$phone=$_POST['phone'];
-$message=$_POST['message'];
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $phone = htmlspecialchars($_POST['phone']);
+    $message = htmlspecialchars($_POST['message']);
 
-// Database creation 
+    // Validate and sanitize inputs
+    // Implement your validation logic for email and phone here
 
-$conn =new mysqli('localhost','root','rohit','contact_form');
-if($conn->connect_error){
-    die('Connection Failed: '.$conn->connect_error );
-}else{
-    $stmt =$conn->prepare("INSERT INTO contact_form(name,email,phone,message) values(?,?,?,?)");
-    $stmt->bind_param("ssss",$name,$email,$phone,$message);
-    $stmt->execute();
-    echo "Message Sent Succesfully....";
-    $stmt->close();
-    $conn->close();
+    $conn = new mysqli('localhost', 'root', '', 'contact_form'); // Replace 'your_username' and 'your_password'
+    
+    if ($conn->connect_error) {
+        die('Connection Failed: ' . $conn->connect_error);
+    } else {
+        $stmt = $conn->prepare("INSERT INTO contact_form(name, email, phone, message) VALUES (?, ?, ?, ?)");
+        $stmt->bind_param("ssss", $name, $email, $phone, $message);
+        if ($stmt->execute()) {
+            echo "Message Sent Successfully....";
+        } else {
+            echo "Error: " . $stmt->error;
+        }
+        $stmt->close();
+        $conn->close();
+    }
 }
-?>
--- connect1.php
+++ connect1.php
@@ -1,18 +1,20 @@
 <?php
 $name = $_POST['name'];
 $phone = $_POST['phone'];
+$email = $_POST['email'];
+$place = $_POST['place'];
 $message = $_POST['message'];
 
-// Database creation 
 
-$conn = new mysqli('localhost', 'root', 'rohit', 'contact_form_2');
+// Database connection
+$conn = new mysqli('localhost', 'root', '', 'contact_form_2');
 if ($conn->connect_error) {
     die('Connection Failed: ' . $conn->connect_error);
 } else {
-    $stmt = $conn->prepare("INSERT INTO contact_form_2(name,phone,message) values(?,?,?,?)");
-    $stmt->bind_param("sss", $name, $phone, $message);
+    $stmt = $conn->prepare("INSERT INTO contact_form_2(name, phone, email, place, message) VALUES (?,?,?, ?, ?)");
+    $stmt->bind_param("sssss", $name, $phone, $email, $place, $message);
     $stmt->execute();
-    echo "Message Sent Succesfully....";
+    echo "Message Sent Successfully....";
     $stmt->close();
     $conn->close();
 }
